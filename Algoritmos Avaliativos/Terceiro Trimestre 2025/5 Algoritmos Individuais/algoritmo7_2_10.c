436432108403ac91f390e3347530a3db2247471c
Arthur Magalhães <arthurmag7@gmail.com>
2025-11-23 19:09:31 -0300

    Update Algoritmo 7.2.10

--- Algoritmos Avaliativos/Terceiro Trimestre 2025/5 Algoritmos Individuais/algoritmo7_2_10.c
+++ Algoritmos Avaliativos/Terceiro Trimestre 2025/5 Algoritmos Individuais/algoritmo7_2_10.c
@@ -4,7 +4,7 @@
 
 //Algoritmo 7.2.10: Em uma pesquisa de campo envolvendo 250 entrevistados, uma editora solicitou os seguintes dados aos entrevistados: sexo, idade e quantidade de livros que leu no ano de 2009. Fazer um programa que leia os dados digitados pelo usuário e ao final, calcule e imprima: a) A quantidade total de livros lidos pelos entrevistados menores de 10 anos. b) A quantidade de mulheres que leram 5 livros ou mais. c) A média de idade dos homens que leram menos que 5 livros. d) O percentual de pessoas que não leram livros.
 
-#define QTD_DE_ENTREVISTADOS 3
+#define QTD_DE_ENTREVISTADOS 250
 
 int main(){
     struct Entrevistado
